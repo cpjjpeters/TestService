09099605791e83edd216978133cb4bd31c9d9910
Carl Peters <31207893+cpjjpeters@users.noreply.github.com>
2018-07-23 20:00:09 +0200

    that is all

--- CsharpRestClient/Form1.Designer.cs
+++ CsharpRestClient/Form1.Designer.cs
@@ -30,36 +30,86 @@
         {
             this.txtRestURL = new System.Windows.Forms.TextBox();
             this.txtResponse = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtRestURL
             // 
-            this.txtRestURL.Location = new System.Drawing.Point(171, 56);
+            this.txtRestURL.Location = new System.Drawing.Point(99, 22);
+            this.txtRestURL.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.txtRestURL.Name = "txtRestURL";
-            this.txtRestURL.Size = new System.Drawing.Size(369, 38);
+            this.txtRestURL.Size = new System.Drawing.Size(196, 20);
             this.txtRestURL.TabIndex = 0;
             this.txtRestURL.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // txtResponse
             // 
-            this.txtResponse.Location = new System.Drawing.Point(171, 145);
+            this.txtResponse.Location = new System.Drawing.Point(99, 62);
+            this.txtResponse.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.txtResponse.Multiline = true;
             this.txtResponse.Name = "txtResponse";
             this.txtResponse.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtResponse.Size = new System.Drawing.Size(588, 38);
+            this.txtResponse.Size = new System.Drawing.Size(380, 194);
             this.txtResponse.TabIndex = 0;
             this.txtResponse.Text = "C#Rest Client";
             this.txtResponse.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(503, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "GO1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Request URL:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Response:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(503, 100);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(632, 365);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.txtResponse);
             this.Controls.Add(this.txtRestURL);
+            this.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.Name = "Form1";
             this.Text = "C# REST Client";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -69,6 +119,10 @@
 
         private System.Windows.Forms.TextBox txtRestURL;
         private System.Windows.Forms.TextBox txtResponse;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- CsharpRestClient/Form1.cs
+++ CsharpRestClient/Form1.cs
@@ -21,5 +21,45 @@ namespace CsharpRestClient
         {
 
         }
+        #region UI Event Handlers
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //DebugOutput("This is some output so I can see what is going on!");
+            RestClient rClient = new RestClient();
+            rClient.endPoint = txtRestURL.Text;
+
+            DebugOutput("Restclient created");
+            string strResponse = String.Empty;
+            strResponse = rClient.makeRequest();
+            DebugOutput(strResponse);
+        }
+
+#endregion
+
+        private void DebugOutput(string strDebugText)
+        {
+            try
+            {
+                System.Diagnostics.Debug.Write(strDebugText + Environment.NewLine);
+                txtResponse.Text = txtResponse.Text + strDebugText + Environment.NewLine;
+                txtResponse.SelectionStart = txtResponse.TextLength;
+                txtResponse.ScrollToCaret();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.Write(ex.Message, ToString() + Environment.NewLine);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtResponse.Text = null;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string strResponse = String.Empty;
+        }
     }
 }
